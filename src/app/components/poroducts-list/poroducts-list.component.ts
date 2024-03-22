b240676d4db6167dee879daf19ac47f814456999
MarS-27 <marchuk1992@gmail.com>
2024-03-22 14:49:23 +0200

    init angular project

---
+++ src/app/components/poroducts-list/poroducts-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poroducts-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './poroducts-list.component.html',
+  styleUrl: './poroducts-list.component.css'
+})
+export class PoroductsListComponent {
+
+}
