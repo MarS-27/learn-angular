6bbd96ee63f35edf37018ec97366ce8109286878
MarS-27 <marchuk1992@gmail.com>
2024-03-26 14:54:23 +0200

    added header and footer

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-
+  currentDate = new Date();
 }
